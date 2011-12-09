4a5e22b5ecd4eb2e374b00a613f39552ee48e87a
vkill <vkill.net@gmail.com>
2011-12-09 21:05:09 +0800

    fix bold.data bug

--- app/models/repo_file_observer.rb
+++ app/models/repo_file_observer.rb
@@ -3,22 +3,25 @@ class RepoFileObserver < ActiveRecord::Observer
   observe :repo_file
 
   def after_create(record)
-    r = Grit::Repo.new record.git_repo_path
-    i = Grit::Index.new(r)
-    i.add(record.repo_file.file.filename, record.repo_file.file.to_file)
+    r = record.git_repo
+    i = r.index
+    i.read_tree "master"
+    i.add(record.repo_file.file.filename, record.repo_file.file.to_file.read.force_encoding(Encoding::UTF_8))
     i.commit "add #{ record.repo_file.file.filename }", r.commits
   end
 
   def after_update(record)
-    r = Grit::Repo.new record.git_repo_path
-    i = Grit::Index.new(r)
-    i.add(record.repo_file.file.filename, record.repo_file.file.to_file)
+    r = record.git_repo
+    i = r.index
+    i.read_tree "master"
+    i.add(record.repo_file.file.filename, record.repo_file.file.to_file.read.force_encoding(Encoding::UTF_8))
     i.commit "update #{ record.repo_file.file.filename }", r.commits
   end
 
   def after_destroy(record)
-    r = Grit::Repo.new record.git_repo_path
-    i = Grit::Index.new(r)
+    r = record.git_repo
+    i = r.index
+    i.read_tree "master"
     i.delete(record.repo_file.file.filename)
     i.commit "delete #{ record.repo_file.file.filename }", r.commits
   end
