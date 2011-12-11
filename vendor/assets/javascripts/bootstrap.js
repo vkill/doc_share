242d89e3b5a8f7dfcad53f11efb6159a18c30380
vkill <vkill.net@gmail.com>
2011-12-11 21:07:39 +0800

    add data-twipsy support

--- vendor/assets/javascripts/bootstrap.js
+++ vendor/assets/javascripts/bootstrap.js
@@ -9,8 +9,68 @@
 
 // enable twipsy
 $(function () {
-  $("a[rel=twipsy]").twipsy({
-    live: true
+  $("a[rel=twipsy]")
+    .twipsy({
+      live: true
+    })
+})
+$(function () {
+  $("a[rel=twipsy][data-disable-click]")
+    .click(function(e) {
+      e.preventDefault()
+    })
+})
+$(function () {
+  $("[data-twipsy]")
+    .twipsy({
+      live: true
+    })
+})
+$(function () {
+  $("[data-twipsy-title-div]").each(function(){
+    $(this).twipsy({
+      live: true,
+      html: true,
+      title: function(){
+        return $(".popover#" + $(this).data("title-div")).html()
+      }
+    })
+  })
+})
+
+
+
+// enable popover on a and data-popover
+$(function () {
+  $("a[rel=popover]")
+    .popover({
+      offset: 10,
+      live: true
+    })
+})
+$(function () {
+  $("a[rel=popover][data-disable-click]")
+    .click(function(e) {
+      e.preventDefault()
+    })
+})
+$(function () {
+  $("[data-popover]")
+    .popover({
+      offset: 10,
+      live: true
+    })
+})
+$(function () {
+  $("[data-popover-content-div]").each(function(){
+    $(this).popover({
+      offset: 10,
+      live: true,
+      html: true,
+      content: function(){
+        return $(".popover#" + $(this).data("content-div")).html()
+      }
+    })
   })
 })
 
@@ -22,7 +82,7 @@ function buttomRequestTimeout(requestId) {
     if (dom.attr('disabled') == 'disabled') {
       $("div#" + dom.data('alerts-containers-div')).append(
         "<div class='alert-message warning' data-alert=true><a class='close' href='#'>×</a><p>" + dom.data('timeout-text') + "</p></div>"
-    )
+      )
       dom.button('reset')
     }
   }
@@ -30,11 +90,14 @@ function buttomRequestTimeout(requestId) {
 $(function() {
   $("[data-buttons]").each(function(){
     btn = $(this).click(function () {
-      if (btn.attr('disabled') == 'disabled') { return false }
-      btn.button('loading')
-      requestId = Math.random() + Math.random() + Math.random()
-      btn.attr("data-request-id", requestId)
-      setTimeout("buttomRequestTimeout(" + requestId + ")", 8000)
+      if (btn.attr('disabled') == 'disabled') {
+        return false
+      } else {
+        btn.button('loading')
+        requestId = Math.random() + Math.random() + Math.random()
+        btn.attr("data-request-id", requestId)
+        setTimeout("buttomRequestTimeout(" + requestId + ")", 8000)
+      }
     })
   })
 })
