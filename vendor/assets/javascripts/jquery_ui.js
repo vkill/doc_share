4617d232ee3540c84c4290ab7e6ac3ccd4d72f51
vkill <vkill.net@gmail.com>
2011-12-17 00:49:40 +0800

    fix date lt gt bug

--- vendor/assets/javascripts/jquery_ui.js
+++ vendor/assets/javascripts/jquery_ui.js
@@ -1,7 +1,10 @@
 // require jquery_ui_1.8.17/jquery-ui
 //= require jquery_ui_1.8.17/jquery.ui.core
+
+//jquery file upload use
 //= require jquery_ui_1.8.17/jquery.ui.widget
 //= require jquery_ui_1.8.17/jquery.ui.progressbar
+
 //= require jquery_ui_1.8.17/jquery.ui.datepicker
 //= require jquery_ui_1.8.17/i18n/jquery.ui.datepicker-zh-CN
 
