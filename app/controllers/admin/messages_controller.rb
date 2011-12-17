c54c7af1c37d558e4b374680ba09092b75c53dd1
vkill <vkill.net@gmail.com>
2011-12-17 23:33:11 +0800

    modify admin topbar

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,2 +1,14 @@
-class Admin::MessagesController < ApplicationController
+class Admin::MessagesController < Admin::ResourcesBaseController
+
+  add_breadcrumb proc{|c| c.t("admin.navigation.messages")}, "", :only => [:index]
+  
+  add_breadcrumb proc{|c| c.t("admin.navigation.roles")}, :admin_users_path, :except => [:index]
+  add_breadcrumb proc{|c| c.t("new")}, "", :only => [:new, :create]
+  add_breadcrumb proc{|c| c.t("export")}, "", :only => [:export]
+
+  add_breadcrumb proc{|c| "#{Role.model_name.human} ##{c.params[:id]}"}, proc{|c| c.admin_role_path},
+                  :except => [:index, :new, :create, :export]
+  add_breadcrumb proc{|c| c.t("show")}, "", :only => [:show]
+  add_breadcrumb proc{|c| c.t("edit")}, "", :only => [:edit, :update]
+  add_breadcrumb proc{|c| c.t("delete")}, "", :only => [:delete, :destroy]
 end
