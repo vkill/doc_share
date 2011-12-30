c584c0029e42e0b2d8e13df6b418863b11380ed2
vkill <vkill.net@gmail.com>
2011-12-30 12:21:08 +0800

    modify mailer i18n

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,40 +6,40 @@ class UserMailer < ActionMailer::Base
     @user = user
     @url = url_for(:controller => :reset_passwords, :action => :edit, :id => user.reset_password_token)
     mail(:to => user.email,
-         :subject => t("sorcery.mailer.reset_password_email.subject"))
+         :subject => t("sorcery.mailer.reset_password_email.subject", :site_name => SiteConfig.q(:site_name)))
   end
 
   def activation_needed_email(user)
     @user = user
     @url = url_for(:controller => :users, :action => :activate, :id => user.activation_token)
     mail(:to => user.email,
-         :subject => t("sorcery.mailer.activation_needed_email.subject"))
+         :subject => t("sorcery.mailer.activation_needed_email.subject", :site_name => SiteConfig.q(:site_name)))
   end
 
   def activation_success_email(user)
     @user = user
     @url = signin_path(:only_path => false)
     mail(:to => user.email,
-         :subject => t("sorcery.mailer.activation_success_email.subject"))
+         :subject => t("sorcery.mailer.activation_success_email.subject", :site_name => SiteConfig.q(:site_name)))
   end
 
   if Rails.env.development?
     class Preview < MailView
 
       def reset_password_email
-        user = User.new(:email => '122755990@qq.com')
+        user = User.new(:email => '122755990@qq.com', :username => 'vkill')
         user.reset_password_token = 'test'
         ::UserMailer.reset_password_email(user)
       end
 
       def activation_needed_email
-        user = User.new(:email => '122755990@qq.com')
+        user = User.new(:email => '122755990@qq.com', :username => 'vkill')
         user.activation_token = 'test'
         ::UserMailer.activation_needed_email(user)
       end
 
       def activation_success_email
-        user = User.new(:email => '122755990@qq.com')
+        user = User.new(:email => '122755990@qq.com', :username => 'vkill')
         ::UserMailer.activation_success_email(user)
       end
 
