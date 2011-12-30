0bef32d98d65dd902146cce9a4b1f18ffab63170
vkill <vkill.net@gmail.com>
2011-12-30 13:59:07 +0800

    search add i18n

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,7 +5,11 @@ class Search
   attr_accessor :type, :q
 
   def self.get_type_values
-    [["users", "users"], ["repositories", "repositories"], ["everything", "everything"]]
+    [
+      [I18n.translate(:"activemodel.enums.search.type.users"), "users"],
+      [I18n.translate(:"activemodel.enums.search.type.repositories"), "repositories"],
+      [I18n.translate(:"activemodel.enums.search.type.everything"), "everything"]
+    ]
   end
 
   validate :build_type
