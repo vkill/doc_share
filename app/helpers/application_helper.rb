40e90ecbbcb089a3759189cb88fac378d0b53dc4
vkill <vkill.net@gmail.com>
2012-01-06 13:56:24 +0800

    fix owner? helper bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
 
   def owner?(target)
-    return false if !target.respond_to?(:user_id) or !current_user.respond_to?(:id)
+    return false if !target.respond_to?(:user_id) or !current_user
     target.user_id == current_user.id
   end
 
   def me?(user)
-    return false if !user.respond_to?(:id) or !current_user.respond_to?(:id)
+    return false if !user.respond_to?(:id) or !current_user
     user.id == current_user.id
   end
 
   def admin?
-    return false if !current_user.respond_to?(:id)
+    return false if !current_user
     current_user.has_role?("admin") or current_user.super_admin?
   end
 
