0773a601ca23819c3b713981b75be5bdb8b9192c
vkill <vkill.net@gmail.com>
2012-01-07 18:34:14 +0800

    drop repositories partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,17 +28,5 @@ module ApplicationHelper
     raw ("&nbsp" * n)
   end
 
-  # def namespace_name_hash
-  #   path_hash = params[:controller].split("/")
-  #   path_hash.pop
-  #   path_hash.map(&:to_sym)
-  # end
-
-  def namespace_name
-    path_hash = params[:controller].split("/")
-    path_hash.pop
-    path_hash.first
-  end
-
 end
 
--- app/helpers/repositories_helper.rb
+++ app/helpers/repositories_helper.rb
@@ -1,2 +1,67 @@
 module RepositoriesHelper
+
+  def user_link(user)
+    raw (link_to user.username, user_page_path(user.username))
+  end
+
+  def repository_link(repository)
+    raw (link_to repository.name, user_repository_path(repository.username, repository.name))
+  end
+
+  def repository_link_full(repository)
+    raw user_link(repository.user) + "/" + repository_link(repository)
+  end
+
+  def repository_watchers_count_link(repository)
+    raw link_to repository.watchers_count,
+                user_repository_watchers_path(repository.username, repository.name),
+                :class => "btn large small",
+                :rel => "twipsy",
+                :data => {:original_title => t(".watchers"),
+                          :placement => :below,
+                          "repository_watchers_count_#{repository.id}" => true}
+  end
+
+  def repository_forks_count_link(repository)
+    raw link_to repository.forks_count,
+                user_repository_forks_path(repository.username, repository.name),
+                :class => "btn large small",
+                :rel => "twipsy",
+                :data => {:original_title => t(".forks"),
+                          :placement => :below,
+                          "repository_forks_count_#{repository.id}" => true}
+  end
+
+  def repository_fork_link(repository)
+    if repository.forked_by_user?(current_user)
+      raw link_to t(".forked"),
+                  user_repository_path(current_user.username, repository.forked_by_user(current_user).name),
+                  :class => "btn small",
+                  :rel => "twipsy",
+                  :data => {:original_title => t(".forked_info"),
+                            :placement => :below}
+    else
+      raw link_to t(".fork"),
+                  user_repository_fork_path(repository.username, repository.name),
+                  :method => :put,
+                  :class => "btn small primary"
+    end
+  end
+
+  def repository_reverse_watch_link(repository)
+    raw link_to (current_user.watching_repository?(repository) ? t(".unwatch") : t(".watch")),
+                user_repository_reverse_watch_path(repository.username, repository.name),
+                :method => :put,
+                :remote => true,
+                :data => { "repository_reverse_watch_#{repository.id}" => true,
+                            :buttons => true,
+                            :loading_text => t(:requesting),
+                            :timeout_text => t(:request_timeout),
+                            :server_error_text => t(:request_server_error),
+                            :alerts_containers_div => "alerts",
+                            :watch_complete_text => t(".unwatch"),
+                            :unwatch_complete_text => t(".watch")},
+                :class => "btn small primary"
+  end
+  
 end
