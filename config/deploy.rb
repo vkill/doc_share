832b958bb2e3caada85029570d523e7402320a78
vkill <vkill.net@gmail.com>
2012-01-12 16:15:51 +0800

    add db:seed and more to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,9 +4,11 @@ require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_ruby_string, '1.9.3@doc_share'        # Or whatever env you want it to run in.
 set :rvm_type, :user
 
+
 # Load bundler's capistrano plugin.
 set :bundle_roles, [:app]
 require "bundler/capistrano"
+
 
 set :application, "doc_share"
 
@@ -31,8 +33,10 @@ set :user, "railsapp"
 
 set :use_sudo, false
 
+
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
+
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
@@ -41,14 +45,30 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+end
+
+
+# copy config files
+namespace :deploy do
   task :copy_config_files, :roles => [:app] do
     run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "cp #{shared_path}/config/redis.yml #{release_path}/config/redis.yml"
     run "cp #{shared_path}/config/smtp_settings.yml #{release_path}/config/smtp_settings.yml"
   end
 end
-
 after "deploy:finalize_update", "deploy:copy_config_files"
+
+
+
+# db:seed
+namespace :deploy do
+  namespace :db do
+    task :seed, :roles => [:db] do
+      run_remote_rake "db:seed"
+    end
+  end
+end
+
 
 # airbrake support
 require './config/boot'
@@ -56,29 +76,20 @@ require 'airbrake/capistrano'
 
 
 
-############# Thinking Sphinx
-############# http://ayaya.tw/blog/2011/11/02/setup-thinking-sphinx-with-chinese-support/
-require 'thinking_sphinx/deploy/capistrano'
-namespace :deploy do
-  namespace :thinking_sphinx do
-    task :rebuild, :roles => :app do
-      run "cd #{release_path} && RAILS_ENV=#{rails_env} bundle exec rake ts:stop ts:rebuild"
-    end
-  end
-  after 'deploy:restart', 'deploy:thinking_sphinx:rebuild'
-end
-
-
 
 ############# resque workers and resque scheduler
 ############# http://balazs.kutilovi.cz/blog/2011/12/04/deploying-resque-scheduler-with-capistrano/
 after "deploy:symlink", "deploy:restart_workers"
-after "deploy:restart_workers", "deploy:restart_scheduler"
-##
-# Rake helper task.
-# http://pastie.org/255489
-# http://geminstallthat.wordpress.com/2008/01/27/rake-tasks-through-capistrano/
-# http://ananelson.com/said/on/2007/12/30/remote-rake-tasks-with-capistrano/
+after "deploy:symlink", "deploy:restart_scheduler"
+
+after "deploy:stop", "deploy:stop_workers"
+after "deploy:start", "deploy:start_workers"
+after "deploy:restart", "deploy:restart_workers"
+
+after "deploy:stop", "deploy:stop_scheduler"
+after "deploy:start", "deploy:start_scheduler"
+after "deploy:restart", "deploy:restart_scheduler"
+
 def run_remote_rake(rake_cmd)
   rake_args = ENV['RAKE_ARGS'].to_s.split(',')
   cmd = "cd #{fetch(:latest_release)} && #{fetch(:rake, "rake")} RAILS_ENV=#{fetch(:rails_env, "production")} #{rake_cmd}"
@@ -87,12 +98,45 @@ def run_remote_rake(rake_cmd)
   set :rakefile, nil if exists?(:rakefile)
 end
 namespace :deploy do
-  desc "Restart Resque Workers"
   task :restart_workers, :roles => :db do
     run_remote_rake "resque:restart_workers"
   end
-  desc "Restart Resque scheduler"
   task :restart_scheduler, :roles => :db do
     run_remote_rake "resque:restart_scheduler"
+  end
+
+  task :stop_workers, :roles => :db do
+    run_remote_rake "resque:stop_workers"
+  end
+  task :stop_scheduler, :roles => :db do
+    run_remote_rake "resque:stop_scheduler"
+  end
+
+  task :start_workers, :roles => :db do
+    run_remote_rake "resque:start_workers"
+  end
+  task :start_scheduler, :roles => :db do
+    run_remote_rake "resque:start_scheduler"
+  end
+end
+
+
+############# Thinking Sphinx
+after 'deploy:symlink', 'deploy:restart_thinking_sphinx'
+
+after 'deploy:restart', 'deploy:restart_thinking_sphinx'
+after 'deploy:stop', 'deploy:stop_thinking_sphinx'
+after 'deploy:start', 'deploy:start_thinking_sphinx'
+namespace :deploy do
+  task :restart_thinking_sphinx, :roles => :db do
+    run_remote_rake "thinking_sphinx:rebuild"
+  end
+
+  task :stop_thinking_sphinx, :roles => :db do
+    run_remote_rake "thinking_sphinx:stop"
+  end
+
+  task :start_thinking_sphinx, :roles => :db do
+    run_remote_rake "thinking_sphinx:start"
   end
 end
