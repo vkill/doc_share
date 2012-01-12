8ebb38d65e2bb5cc7501504ec3c0400c198273e0
vkill <vkill.net@gmail.com>
2012-01-12 16:32:59 +0800

    add 500 404 en.html.haml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
     rescue_from Exception,                            :with => :render_error
     rescue_from RuntimeError,                         :with => :render_error
     rescue_from ActiveRecord::RecordNotFound,         :with => :render_404
-    # rescue_from ActionController::RoutingError,       :with => :render_404
+    rescue_from ActionController::RoutingError,       :with => :render_404
     rescue_from ActionController::UnknownController,  :with => :render_error
     rescue_from ActionController::UnknownAction,      :with => :render_error
   end
